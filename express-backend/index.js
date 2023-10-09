55004f26f4f0482df502cf0b79ff97b474d72a50
rrn9902 <nair.rahul992@gmail.com>
2023-10-08 23:33:57 -0700

    linking p1

--- express-backend/index.js
+++ express-backend/index.js
@@ -1,5 +1,6 @@
 // backend.js
 import express from "express";
+import cors from 'cors';
 
 const app = express();
 const port = 8000;
@@ -35,6 +36,7 @@ const users = {
    ]
 };
 
+app.use(cors());
 app.use(express.json());
 
 app.get('/', (req, res) => {
